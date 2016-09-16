2d13363fb48ab40e90c432e4152bd8db6764f7bb
Morgan Paki <morgan@psdesignstudio.com>
2016-09-16 13:41:40 +1000

    Updated service provider

--- src/Flash/FlashServiceProvider.php
+++ src/Flash/FlashServiceProvider.php
@@ -6,42 +6,42 @@ use Illuminate\Support\ServiceProvider;
 
 class FlashServiceProvider extends ServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->bind(
-            'Laracasts\Flash\SessionStore',
-            'Laracasts\Flash\LaravelSessionStore'
-        );
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bind(
+			'PSDesign\Flash\SessionStore',
+			'PSDesign\Flash\LaravelSessionStore'
+		);
 
-        $this->app->singleton('flash', function () {
-            return $this->app->make('Laracasts\Flash\FlashNotifier');
-        });
-    }
+		$this->app->singleton('flash', function () {
+			return $this->app->make('PSDesign\Flash\FlashNotifier');
+		});
+	}
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->loadViewsFrom(__DIR__ . '/../../views', 'flash');
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->loadViewsFrom(__DIR__ . '/../views', 'flash');
 
-        $this->publishes([
-            __DIR__ . '/../../views' => base_path('resources/views/vendor/flash')
-        ]);
-    }
+		$this->publishes([
+			__DIR__ . '/../views' => base_path('resources/views/vendor/flash')
+		]);
+	}
 
 }
